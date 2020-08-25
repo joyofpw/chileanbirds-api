1cd9ad5d211bde1131bd6d9322c68679009c473d
Camilo Castro <camilo@ninjas.cl>
2020-08-25 15:20:58 -0400

    added more info to modals

--- src/frontend/src/pages/home/components/birdmodal.jsx
+++ src/frontend/src/pages/home/components/birdmodal.jsx
@@ -17,9 +17,30 @@ export default ({ bird, setOpenModal }) => (
           </div>
           <div className="media-content">
             <div className="content">
-              <h2 className="title is-2">{bird.name.latin}</h2>
+              <h3 className="title is-3">{bird.name.latin}</h3>
+              <h4 className="title is-4">
+                {bird.name.spanish} | {bird.name.english}
+              </h4>
+
               <p>{bird.didyouknow}</p>
               <p>{bird.habitat}</p>
+
+              <p>{bird.iucn.title}</p>
+              <p>{bird.iucn.description}</p>
+
+              <p>{bird.order}</p>
+              <p>{bird.size}</p>
+              <p>{bird.species}</p>
+
+              <figure className="image is-4by3">
+                <img
+                  src={bird.map.image}
+                  alt={bird.map.title}
+                  title={bird.map.title}
+                />
+              </figure>
+
+              <p>{bird.map.title}</p>
             </div>
           </div>
         </article>
