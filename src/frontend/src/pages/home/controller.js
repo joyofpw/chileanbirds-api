d719fb07e1361286f4ad4b346ccb2cd8800ed857
Camilo Castro <camilo@ninjas.cl>
2020-08-26 12:55:18 -0400

    improved search

--- src/frontend/src/pages/home/controller.js
+++ src/frontend/src/pages/home/controller.js
@@ -1,31 +1,30 @@
-import React, { useState, useEffect } from "react";
-import View, { BirdModal } from "./view";
-import Model from "./model";
+import React, { useState, useEffect } from 'react';
+import View, { BirdModal } from './view';
+import Model from './model';
 
 export default () => {
   const [birds, setBirds] = useState(Model.all.default);
-
   const [cachedBirds, setCachedBirds] = useState(Model.all.default);
 
   const [openModal, setOpenModal] = useState(null);
-
   const [birdModal, setBirdModal] = useState(null);
 
   const updateSearch = ({ target: { value } }) => {
     const query = value.trim().toLowerCase();
 
-    if (query === "") {
+    if (query === '') {
       setCachedBirds(birds);
       return;
     }
 
     const filtered = birds.items.filter((bird) => {
       const text = [
+        ' ', // Add initial space to allow better search experience
         bird.name.spanish.toLowerCase(),
-        bird.name.english.toLowerCase().split("-").join(" "),
+        bird.name.english.toLowerCase().split('-').join(' '),
         bird.name.latin.toLowerCase(),
-        bird.uid.split("-").join(" "),
-      ].join(" ");
+        bird.uid.split('-').join(' ')
+      ].join(' ');
 
       return text.indexOf(query) > 0 || text.search(query) > 0;
     });
