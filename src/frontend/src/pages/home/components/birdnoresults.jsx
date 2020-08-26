d9c7eb8dad564702435b75941fce987251b7111b
Camilo <camilo.castro.cabrera@gmail.com>
2020-08-25 22:59:43 -0400

    ref(frontend): refactored birds (#12)
    closes #10

---
+++ src/frontend/src/pages/home/components/birdnoresults.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default () => (
+  <section className="container">
+    <div className="columns">
+      <div className="column is-12">
+        <div className="content has-text-centered">
+          <span className="icon is-large">
+            <h1
+              className="is-title"
+              style={{ fontSize: '5em', paddingTop: '3em' }}
+            >
+              <i className="fa fa-exclamation-triangle"></i>
+            </h1>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+);
--- src/frontend/src/pages/home/components/birds.jsx
+++ src/frontend/src/pages/home/components/birds.jsx
@@ -3,70 +3,9 @@ import InfiniteScroller from 'react-infinite-scroller';
 import { Spinner } from '../../../shared/components';
 import { usePromiseTracker } from 'react-promise-tracker';
 
-const makeRow = (cols, index) => (
-  <div key={index} className="columns features">
-    {cols}
-  </div>
-);
-
-const makeColumnItem = (
-  item,
-  index,
-  { openModal, setOpenModal, birdModal }
-) => {
-  return (
-    <React.Fragment key={index}>
-      <div
-        className="column is-3"
-        onClick={() => setOpenModal(item.uid)}
-        style={{ cursor: 'pointer' }}
-      >
-        <div className="card is-shady">
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img
-                src={item.images.main}
-                alt={item.name.latin}
-                title={item.name.spanish}
-                className="modal-button"
-              />
-            </figure>
-          </div>
-          <div className="card-content">
-            <div className="content">
-              <h3 className="title is-3">{item.name.latin}</h3>
-              <p>
-                {item.name.spanish} / {item.name.english}
-              </p>
-              {openModal === item.uid ? <Spinner /> : null}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {openModal === item.uid ? birdModal : null}
-    </React.Fragment>
-  );
-};
-
-const noResults = () => (
-  <section className="container">
-    <div className="columns">
-      <div className="column is-12">
-        <div className="content has-text-centered">
-          <span className="icon is-large">
-            <h1
-              className="is-title"
-              style={{ fontSize: '5em', paddingTop: '3em' }}
-            >
-              <i className="fa fa-exclamation-triangle"></i>
-            </h1>
-          </span>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+import makeColumnItem from './birdcolumn';
+import makeRow from './birdrow';
+import noResults from './birdnoresults';
 
 export default ({ birds, openModal, setOpenModal, birdModal }) => {
   if (!birds) {
